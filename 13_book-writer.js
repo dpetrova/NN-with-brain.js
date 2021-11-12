262e6c1924bf5fa3334fa36ebbc4b9ab166fadaa
dpetrova <daniella_petrova@abv.bg>
2021-11-12 11:52:41 +0200

    text prediction

---
+++ 13_book-writer.js
@@ -0,0 +1,21 @@
+/* WRITING CHILDREN BOOKS */
+
+const brain = require("brain.js");
+
+const trainingData = [
+  "Jane saw Doug.",
+  "Doug saw Jane.",
+  "Spot saw Doug and Jane looking at each other.",
+  "It was love at first sight, and Spot had a frontrow seat. It was a very special moment for all.",
+];
+
+const net = new brain.recurrent.LSTM();
+
+net.train(trainingData, {
+  iterations: 1500,
+  errorThresh: 0.011,
+  //log: (stats) => console.log(stats),
+});
+
+console.log(net.run("Jane")); // saw Doug.
+console.log(net.run("It was")); // love at first sight, and Spot had a frontrow seat. It was a very special moment for all.
